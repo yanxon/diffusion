2ffff3c1bab6f94fd1664f6ca15354084ccc4d89
Yanxon <yanxon1@stevee.llnl.gov>
2019-07-22 14:32:48 -0700

    update

--- msd.py
+++ msd.py
@@ -2,42 +2,68 @@ import os
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+import sympy
 import subprocess
 
 N = 100
 
-MSD = []
-STEP = []
-TEMP = []
-
-for n in range(N):
-    p = subprocess.Popen(['lmp_serial', '-in', 'diff.in'], stdout=subprocess.PIPE)
-    stdout = p.communicate()[0]
-    rc = p.returncode
-    if rc != 0:
-        error_msg = "LAMMPS exited with return code %d" %rc
-        print(error_msg)
+def MSD(N):
+    primes = list(sympy.primerange(500, 2000))
     
-    step = []
-    temp = []
-    msd = []
-    log = open("log.lammps")
-    for i, line in enumerate(log):
-        if i > 83 and i <= 83+(10000+1): # 10000 = runtime/step
-            arr = re.split(r'\s+', line)
-            temperature = float(arr[2])
-            step.append(float(arr[1])*0.001*1e-3) # 0.001 => tstep, 1e-3 to ns
-            temp.append(float(arr[2]))
-            msd.append(float(arr[3]))
-    log.close()
-    os.remove("log.lammps")
+    MSD = []
+    STEP = []
+    TEMP = []
 
-    MSD.append(msd)
-    STEP.append(step)
-    TEMP.append(temp)
+    for n in range(N):
+        directory = 'hydrogen_'+str(primes[n])
+        os.mkdir(directory)
 
-m_msd = np.mean(MSD, axis=0)
+        string_of_text = []
+        with open("diff.in") as f:
+            for line in f:
+                if line[:15] == 'dump 1 2 custom':
+                    line = f"dump 1 2 custom 100 {directory}/*.chkpt id type mass x y z\n"
+                elif line[:27] == "variable Srandomseed equal ":
+                    line = f"variable Srandomseed equal {primes[n]}\n"
+                string_of_text.append(line)
 
-np.savetxt('msd_paths.txt', MSD)
-plt.plot(step, m_msd)
-plt.savefig('msdvstime.png', dpi=1000)
+        with open("diff.in", 'w') as f:
+            for line in string_of_text:
+                f.write(line)
+
+        p = subprocess.Popen(['lmp_serial', '-in', 'diff.in'], stdout=subprocess.PIPE)
+        stdout = p.communicate()[0]
+        rc = p.returncode
+        if rc != 0:
+            error_msg = "LAMMPS exited with return code %d" %rc
+            print(error_msg)
+
+        step = []
+        temp = []
+        msd = []
+        log = open("log.lammps")
+        for i, line in enumerate(log):
+            if i > 130 and i <= 80+(10000): # 10000 = runtime/step
+                arr = re.split(r'\s+', line)
+                temperature = float(arr[2])
+                step.append(float(arr[1])*0.001*1e-3) # 0.001 => tstep, 1e-3 to ns
+                temp.append(float(arr[2]))
+                msd.append(float(arr[3]))
+
+        log.close()
+        os.remove("log.lammps")
+
+        MSD.append(msd)
+        STEP.append(step)
+        TEMP.append(temp)
+
+    m_msd = np.mean(MSD, axis=0)
+
+    np.savetxt('results/msd_paths.txt', MSD)
+    plt.plot(step, m_msd)
+    plt.xlabel('time (ns)')
+    plt.ylabel('MSD (A^2)')
+    plt.savefig('results/msdvstime.png', dpi=1000)
+
+if __name__ == '__main__':
+    MSD(N)
